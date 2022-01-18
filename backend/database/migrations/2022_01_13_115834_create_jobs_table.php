cde0d5c9bd612a443b8bb2a366f2ac83dc5d1ea1
Siwek <norbert.siwek@live.zs1mm.edu.pl>
2022-01-18 15:28:43 +0100

    back v1

--- backend/database/migrations/2022_01_13_115834_create_jobs_table.php
+++ backend/database/migrations/2022_01_13_115834_create_jobs_table.php
@@ -14,7 +14,7 @@ class CreateJobsTable extends Migration
     public function up()
     {
         Schema::create('jobs', function (Blueprint $table) {
-            $table->id();
+            $table->id('id');
             $table->string('name');
             $table->string('description');
             $table->boolean('permanent');
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -26,26 +26,10 @@ Route::get('/articles/{id}', [ArticleController::class, 'readArticle']);
 Route::delete('/articles/{id}', [ArticleController::class, 'deleteArticle']);
 
 Route::post('/articles', [ArticleController::class, 'createArticle']);
-
-Route::put('/v1/jobs/{id}', [JobController::class, 'editJob']);
-
-Route::post('/v1/jobs', [JobController::class, 'addJob']);
-
-Route::delete('/v1/jobs/{id}', [JobController::class, 'deleteJob']);
-
-Route::get('/v1/jobs/{id}', [JobController::class, 'showJob']);
-
-Route::get('/v1/users', [JobController::class, 'showEmployer']);
-
-Route::get('/v1/login', [{]JobController::class, 'authorize']);
-
-Route::post('/v1/register', [JobController::class, 'register']);
-
-Route::get('/v1/search', [JobController::class, 'search']);;
 */
 
 
-Route::put('v1/jobs/{id}', function(Request $request, $id) { //edytuj joba 
+Route::put('api/v1/jobs/{id}', function(Request $request, $id) { //edytuj joba 
     //return Jobs::find($id);
     $job = Jobs::findOrFail($id);
     $job->update($request->all());
@@ -54,71 +38,39 @@ Route::put('v1/jobs/{id}', function(Request $request, $id) { //edytuj joba
 
 
 
-Route::get('v1/jobs', function() { //wyświetl wszystkie jobsy
+Route::get('api/v1/jobs', function() { //wyświetl wszystkie jobsy
     return Jobs::all();
 });
 
 
 
-Route::post('v1/jobs', function(Request $request) { //dodaj joba
+Route::post('api/v1/jobs', function(Request $request) { //dodaj joba
     Jobs::create($request->all);
     return 201;
 });
 
 
 
-Route::delete('v1/jobs/{id}', function($id) { //usun job 
+Route::delete('api/v1/jobs/{id}', function($id) { //usun job 
     Jobs::find($id)->delete();
     return 204;
 });
 
 
 
-Route::get('v1/jobs/{id}', function($id) { //wyświetl konkretny job
+Route::get('api/v1/jobs/{id}', function($id) { //wyświetl konkretny job
     return Jobs::find($id);
 });
 
 
 
-Route::get('v1/users', function() { //wyświetl pracodawców
-    return Users::all();
+Route::get('api/v1/users', function() { //wyświetl pracodawców
+    return users::all();
 });
 
 // rejestracja i logowanie w tym miejscu do zrobienia
 
 
-Route::get('v1/search', function() {
+Route::get('api/v1/search', function() {
     Jobs::find($name)->return;
 });
-
-
-/*
-Use App\Article;
- 
-Route::get('articles', function() {
-    // If the Content-Type and Accept headers are set to 'application/json', 
-    // this will return a JSON structure. This will be cleaned up later.
-    return Article::all();
-});
- 
-Route::get('articles/{id}', function($id) {
-    return Article::find($id);
-});
-
-Route::post('articles', function(Request $request) {
-    return Article::create($request->all);
-});
-
-Route::put('articles/{id}', function(Request $request, $id) {
-    $article = Article::findOrFail($id);
-    $article->update($request->all());
-
-    return $article;
-});
-
-Route::delete('articles/{id}', function($id) {
-    Article::find($id)->delete();
-
-    return 204;
-})
-*/
